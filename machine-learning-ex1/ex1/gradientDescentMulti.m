d0fa2580e8e089855a7339dc29df0faea4e32214
Francisco Farinha <fquico1999@gmail.com>
2019-11-06 10:15:30 -0800

    Completed additional exercises

--- machine-learning-ex1/ex1/gradientDescentMulti.m
+++ machine-learning-ex1/ex1/gradientDescentMulti.m
@@ -16,8 +16,9 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
     %
-
-
+    h = (theta'*X')';
+    delta = ((h-y)'*X)'/m;
+    theta = theta - alpha*delta;
 
 
 
