536b02e8bc326658d4a4c3476123d7f2f7ea8138
Francisco Farinha <fquico1999@gmail.com>
2019-11-06 09:44:32 -0800

    implemented gradient descent using vectorizaiton

--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -16,9 +16,9 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
-
-
+    h = (theta'*X')'; %Compute hypothesis. yields an mx1 matrix. 
+    delta = ((h-y)'*X)'/m; %X is mxn, (h-y) is mx1, so take ((h-y)'*X)' to get nX1 matrix.
+    theta = theta - alpha*delta;
 
 
 
